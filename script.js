5389caa00aae4bc9f004827dac5e9342ef0e51a1
Petn2 <petn.pdx@gmail.com>
2021-12-04 00:21:25 -0800

    best attempt of file recovery
    computer died and lost code that was local to it, did my best to get it close to what it was pre-computer dying

---
+++ script.js
@@ -0,0 +1,33 @@
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.tabTarget)
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('active')
+    })
+    tabs.forEach(tab => {
+      tab.classList.remove('active')
+    })
+    tab.classList.add('active')
+    target.classList.add('active')
+  })
+})
+
+window.onscroll = function() {myFunction()};
+
+
+var navbar = document.getElementById("navbar");
+
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+
